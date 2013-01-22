e9d9f4afb67fddbaa5e523e3504ce896a40f7b04
Heather Krause <hthr.krause@gmail.com>
2013-01-21 21:06:13 -0500

    Added a space to the table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122000806) do
+ActiveRecord::Schema.define(:version => 20130122013540) do
 
   create_table "diary_entries", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "user_name"
   end
 
 end
