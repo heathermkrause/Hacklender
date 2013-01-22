a464be1a72cc4f066f67a1245f1d2b865fb5a189
Heather Krause <hthr.krause@gmail.com>
2013-01-21 20:07:29 -0500

    Initial commit of hacklender

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hacklender::Application.config.secret_token = 'ecc36019be04781749f42846f254efe5f43c8b5c7d404710eac22901c20150832fb9f8b6e48d3f8152c07cba36d04ce53a85c57d772d1e7d937567001ca6cca2'
