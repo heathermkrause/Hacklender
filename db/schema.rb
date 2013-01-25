711e3a87a233122a08731b6b9347f9221b35463d
Heather Krause <hthr.krause@gmail.com>
2013-01-24 20:23:24 -0500

    adding mood

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122013540) do
+ActiveRecord::Schema.define(:version => 20130125011359) do
 
   create_table "diary_entries", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130122013540) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "user_name"
+    t.string   "mood"
   end
 
 end
