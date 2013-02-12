364ccf43a2e2c35653a2a4d29e2f5f7b9b81a97c
Heather Krause <hthr.krause@gmail.com>
2013-02-11 20:55:40 -0500

    Updated the validation

--- app/models/diary_entry.rb
+++ app/models/diary_entry.rb
@@ -1,3 +1,18 @@
 class DiaryEntry < ActiveRecord::Base
   attr_accessible :description, :name, :user_name, :mood
+  validates :name, presence: true
+  validates :mood, :inclusion => { :in => %w(Happy Sad Angry Tired),
+    :message => "%{value} is not a valid mood" }
+  validates :name, :uniqueness => true
+  validate :mood_must_be_happy
 end
+
+
+def mood_must_be_happy
+    if mood !="Happy" && (Time.now.monday? || Time.now.thursday?)
+      errors.add(:mood_must_be_happy, "Mondays must be happy")
+    end
+  end
+
+
+
